50183e85e0203f2dbe2cf04e3c1eac300b6fcab5
Mark Richardson <hand_e_food@hotmail.com>
2020-06-29 09:07:34 +1000

    Damage math.

--- PsiFi/PsiFi/Models/Mapping/Damage.cs
+++ PsiFi/PsiFi/Models/Mapping/Damage.cs
@@ -1,6 +1,14 @@
-﻿namespace PsiFi.Models.Mapping
+﻿using System;
+
+namespace PsiFi.Models.Mapping
 {
-    class Damage
+    /// <summary>
+    /// A specific amount of damage.
+    /// </summary>
+    /// <remarks>
+    /// Used for received damage and damage resistance.
+    /// </remarks>
+    struct Damage
     {
         /// <summary>
         /// The possible range of damage applied.
@@ -21,6 +29,26 @@
         {
             Amount = amount;
             Type = type;
+        }
+
+        public static Damage operator +(Damage damage, int amount) => new Damage(Math.Max(0, damage.Amount + amount), damage.Type);
+
+        public static Damage operator -(Damage damage, int amount) => new Damage(Math.Max(0, damage.Amount - amount), damage.Type);
+
+        /// <summary>
+        /// Applies damage resistance to received damage.
+        /// </summary>
+        /// <param name="damage">The damage received.</param>
+        /// <param name="resistance">The damage resistance.</param>
+        /// <returns>
+        /// If the received damage and damage resistance are of the same type, returns a new damage amount equal to their difference (minimum zero.)
+        /// If the received damage and damage resistance are of different types, returns <paramref name="damage"/> unchnaged.
+        /// </returns>
+        public static Damage operator -(Damage damage, Damage resistance)
+        {
+            return damage.Type == resistance.Type
+                ? damage - resistance.Amount
+                : damage;
         }
     }
 }
--- PsiFi/PsiFi/Models/Mapping/DamageType.cs
+++ PsiFi/PsiFi/Models/Mapping/DamageType.cs
@@ -6,5 +6,6 @@
         Heat,
         Electric,
         Toxic,
+        Psionic,
     }
 }
