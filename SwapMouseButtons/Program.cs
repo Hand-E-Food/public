6eff0807b0c2fc6e532a1ea07aa5d9d220ba0ffa
Mark Richardson <hand_e_food@hotmail.com>
2023-12-16 10:14:00 +1100

    SwapMouseButtons: initial commit

---
+++ SwapMouseButtons/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+[DllImport("user32.dll")]
+static extern int SwapMouseButton(int bSwap);
+
+int rightButtonIsAlreadyPrimary = SwapMouseButton(1);
+if (rightButtonIsAlreadyPrimary != 0)
+    SwapMouseButton(0);
