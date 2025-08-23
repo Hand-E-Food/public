ecb9f32a05710e39d4da38a1fc2120ace9636f5c
Mark Richardson <hand_e_food@hotmail.com>
2025-08-24 09:00:50 +1000

    Letter Boxed Solver: Move logic into class.

--- python/letter-boxed/solve.py
+++ python/letter-boxed/solve.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
 
 import random
-from typing import Any, Callable, Dict, Iterable, List, Optional
+from typing import Dict, Iterable, List, Optional
 
+
+def main(puzzle: Optional[List[str]] = None) -> None:
+    if not puzzle:
+        puzzle = create_puzzle()
+    print(' / '.join(puzzle))
+    words = load_words()
+    solutions = Solver(puzzle, words).solve()
+    if solutions:
+        for solution in solutions:
+            print(' - '.join(solution.words))
+    else:
+        print('No solution')
+
+def create_puzzle() -> List[str]:
+    side_count = 4
+    side_length = 3
+    letter_count = side_count * side_length
+    letters = [letter for letter in 'abcdefghijklmnbopqrstuvwxyz']
+    random.shuffle(letters)
+    letters = ['y', *letters]
+    letters = ''.join(letters[:letter_count])
+    return [letters[n:n+side_length] for n in range(0, letter_count, side_length)]
+
+def load_words() -> Iterable[str]:
+    with open('words.txt', 'r') as file:
+        return (line.strip() for line in file.readlines())
 
 class Chain:
     def __init__(self, words: List[str]) -> None:
@@ -15,52 +41,59 @@ class Chain:
     def __add__(self, word: str) -> 'Chain':
         return Chain([*self.words, word])
 
-def load_words() -> Iterable[str]:
-    with open('words.txt', 'r') as file:
-        return (line.strip() for line in file.readlines())
+class Solver:
+    def __init__(self, puzzle: List[str], words: Iterable[str]) -> None:
+        self.puzzle = puzzle
+        self.puzzle_letters = get_unique_letters(puzzle)
+        if len(self.puzzle_letters) < sum(len(side) for side in puzzle):
+            raise ValueError('puzzle must contain only unique letters')
+        self.words = sorted((word for word in words if self.is_valid_word(word)), key=lambda word: len(word))
+        self.words_starting_with = self._group_by_starting_letter()
 
-def sort_key(word: str) -> Any:
-    return len(word)
+    def is_valid_word(self, word: str) -> bool:
+        if len(word) < 3: return False
+        prev_side = ''
+        for letter in word:
+            try:
+                this_side = next(side for side in self.puzzle if side != prev_side and letter in side)
+                prev_side = this_side
+            except StopIteration:
+                return False
+        return True
 
-def is_valid_word(puzzle: List[str], word: str) -> bool:
-    if len(word) < 3: return False
-    prev_side = ''
-    for letter in word:
-        try:
-            this_side = next(side for side in puzzle if side != prev_side and letter in side)
-            prev_side = this_side
-        except StopIteration:
-            return False
-    return True
+    def _group_by_starting_letter(self) -> Dict[str, List[str]]:
+        words_starting_with: Dict[str, List[str]] = {letter: [] for letter in self.puzzle_letters}
+        for word in self.words:
+            words_starting_with[word[0]].append(word)
+        return words_starting_with
 
-def group_by_starting_letter(letters: str, words: Iterable[str]) -> Dict[str, List[str]]:
-    words_starting_with: Dict[str, List[str]] = {letter: [] for letter in letters}
-    for word in words:
-        words_starting_with[word[0]].append(word)
-    return words_starting_with
+    def solve(self) -> List[Chain]:
+        if self.puzzle_letters != get_unique_letters(self.words):
+            return []
+        chains = [Chain([word]) for word in self.words]
+        print(f'Trying 1 word...')
+        return self._chain_words(chains)
 
-def chain_words(puzzle_letters: str, words_starting_with: Dict[str, List[str]], chains: List[Chain]) -> List[Chain]:
-    if not chains:
-        return []
-    solutions = list(filter(is_solution_for(puzzle_letters), chains))
-    if solutions:
-        return solutions
-    extended_chains: Dict[str, Chain] = {}
-    count = len(chains[0].words) + 1
-    print(f'Trying {count} words...')
-    for chain in chains:
-        words: Iterable[str] = words_starting_with[chain.last_letter]
-        for word in words:
-            extended_chain = chain + word
-            key = extended_chain.key
-            if key not in extended_chains or extended_chains[key].length > extended_chain.length:
-                extended_chains[key] = extended_chain
-    return chain_words(puzzle_letters, words_starting_with, list(extended_chains.values()))
+    def _chain_words(self, chains: List[Chain]) -> List[Chain]:
+        if not chains:
+            return []
+        solutions = list(sorted(filter(self.is_solution, chains), key=lambda solution: solution.length))
+        if solutions:
+            return solutions
+        extended_chains: Dict[str, Chain] = {}
+        count = len(chains[0].words) + 1
+        print(f'Trying {count} words...')
+        for chain in chains:
+            words: Iterable[str] = self.words_starting_with[chain.last_letter]
+            for word in words:
+                extended_chain = chain + word
+                key = extended_chain.key
+                if key not in extended_chains or extended_chains[key].length > extended_chain.length:
+                    extended_chains[key] = extended_chain
+        return self._chain_words(list(extended_chains.values()))
 
-def is_solution_for(puzzle_letters: str) -> Callable[[Chain], bool]:
-    def is_solution(chain: Chain) -> bool:
-        return puzzle_letters == chain.used_letters
-    return is_solution
+    def is_solution(self, chain: Chain) -> bool:
+        return self.puzzle_letters == chain.used_letters
 
 def get_unique_letters(words: List[str]) -> str:
     letters = set()
@@ -69,37 +102,9 @@ def get_unique_letters(words: List[str]) -> str:
             letters.add(letter)
     return ''.join(sorted(letters))
 
-def solve(puzzle: List[str]) -> List[Chain]:
-    words = load_words()
-    valid_words = sorted((word for word in words if is_valid_word(puzzle, word)), key=sort_key)
-    puzzle_letters = get_unique_letters(puzzle)
-    if puzzle_letters != get_unique_letters(valid_words):
-        return []
-    words_starting_with = group_by_starting_letter(puzzle_letters, valid_words)
-    chains = [Chain([word]) for word in valid_words]
-    print(f'Trying 1 word...')
-    return chain_words(puzzle_letters, words_starting_with, chains)
-
-def create_puzzle() -> List[str]:
-    side_count = 4
-    side_length = 3
-    letter_count = side_count * side_length
-    letters = [letter for letter in 'abcdefghijklmnbopqrstuvwxyz']
-    random.shuffle(letters)
-    letters = ['y', *letters]
-    letters = ''.join(letters[:letter_count])
-    return [letters[n:n+side_length] for n in range(0, letter_count, side_length)]
-
-def main(puzzle: Optional[List[str]] = None) -> None:
-    if not puzzle:
-        puzzle = create_puzzle()
-    print(' / '.join(puzzle))
-    solutions = solve(puzzle)
-    if solutions:
-        for solution in solutions:
-            print(' - '.join(solution.words))
-    else:
-        print('No solution')
-
 if __name__ == '__main__':
-    main()
+    puzzle = None
+    #puzzle = ['yye', 'jmi', 'ncr', 'wxs']
+    #puzzle = ['ymz', 'jfc', 'ohl', 'rtb']
+    #puzzle = ['yzl', 'ufj', 'yhx', 'ces']
+    main(puzzle)
